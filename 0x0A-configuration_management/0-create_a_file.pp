f2ee67504203db576318d7c21352dec54ff23733
Yassine <yassinemoussafir31@gmail.com>
2024-04-19 07:21:45 -0700

    0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 file { '/tmp/school':
-  ensure  => 'file',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0744',
-  content => "I love Puppet\n",
+ensure  => present,
+path    => '/tmp/school',
+mode    => '0744',
+owner   => www-data,
+group   => www-data,
+content => 'I love Puppet',
 }
